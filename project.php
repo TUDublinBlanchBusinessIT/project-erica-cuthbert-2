b3c5002d8b36d93b8ef55c9658cbc420b62bdd71
Erica Cuthbert <B00162515@mytudublin.ie>
2025-11-28 09:41:56 +0000

    table of 10 added

--- project.php
+++ project.php
@@ -4,3 +4,19 @@ $username = "root";
 $password = "pass";
 $dbname = "createTennisClubDB.sql";
 $port = 3306;
+
+
+      echo "<TABLE border='1'>";
+
+      for ($i = 0; $i <= 4; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j <= 4; $j++) {
+          echo "<td>$i, $j</td>";
+        }
+        echo "</tr>";
+      }
+
+      echo "</TABLE>";
+    ?>
+  </body>
+</html>
