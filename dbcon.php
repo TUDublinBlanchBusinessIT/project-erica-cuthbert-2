41991081eb2d253510285f81805cb4a80875e696
B00162515 Erica Cuthbert <B00162515@mytudublin.ie>
2025-12-10 10:24:18 +0000

    dbcon updated

--- dbcon.php
+++ dbcon.php
@@ -1,13 +1,27 @@
+
+//$servername = "localhost";
+//$username = "root";
+//$password = "db2025";
+//$dbname = "database1";
+//$port = 3307;
+
+//$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+//if (!$conn){
+   // die("Failed: " . mysqli_connect_error());
+//}
+
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "db2025";
-$dbname = "database1";
+$password = "pass";
+$dbname = "gp_surgery";
 $port = 3307;
 
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-if (!$conn){
-    die("Failed: " . mysqli_connect_error());
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 
+?>
